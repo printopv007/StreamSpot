2841647d707f630033d8a0b56c143c74e9210b17
Printo P Varghese <printopv007@gmail.com>
2024-11-23 12:34:08 +0530

    filter for navigation component

--- src/app/utils/navbar/navbar.component.ts
+++ src/app/utils/navbar/navbar.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { ShowserviceService } from 'src/app/showservice.service';
-import { database } from 'src/assets/data';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  // workweekId = input<number>();
   constructor(private show:ShowserviceService,private auth:AuthService){}
   data:any[]=this.show.getData();
+  navData =[
+    {name:'Comedy',url:'/comedy'},
+    {name:'Sci-Fi',url:'/scifi'},
+    {name:'Horror',url:'/horror'},
+    {name:'Romance',url:'/romance'},
+    {name:'Thriller',url:'/thrill'},
+    {name:'Drama',url:'/drama'},
+    {name:'Mystery',url:'/mystery'},
+    {name:'Crime',url:'/crime'},
+    {name:'Animation',url:'/anime'},
+    {name:'Adventure',url:'/advnt'},
+    {name:'Fantasy',url:'/fantasy'},
+    {name:'Action',url:'/action'}
+  ]
   signout(){
     this.auth.logout();
   }
+ 
 
 
 }
